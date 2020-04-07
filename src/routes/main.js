fd355d0be07357774a8fac839a9fc361b5b554d1
swaggerzinho <63275553+swaggerzinho@users.noreply.github.com>
2020-04-07 12:11:19 -0300

    Update main.js

--- src/routes/main.js
+++ src/routes/main.js
@@ -5,7 +5,7 @@ const route = router.get('/', (req, res, next) => {
   res.status(200).send({
     title: "Music Streaming API",
     version: "0.0.1",
-    author: "Breno Guerra",
+    author: "Swaggerzinho",
     description: "API simples desenvolvida com base no aplicativo de streaming musical Spotify."
   });
 });
